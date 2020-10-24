61cb0ec55d5e403dca2cb6c1d08fe1f4632f23db
Veronika Kruglikova <krveronika@yandex.ru>
2020-10-24 14:45:59 +0300

    reverse через for

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -9,59 +9,59 @@ namespace task {
     // Your code here...
     
     const double EPS_TASK = 1e-7;
-    const std::vector<double> operator+(const std::vector<double> &v1,
-                                        const std::vector<double> &v2) {
+    std::vector<double> operator+(const std::vector<double> &v1,
+                                  const std::vector<double> &v2) {
         std::vector<double> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = v1[i] + v2[i];
         return res;
     }
     
-    const std::vector<int> operator+(const std::vector<int> &v1,
-                                     const std::vector<int> &v2) {
+    std::vector<int> operator+(const std::vector<int> &v1,
+                               const std::vector<int> &v2) {
         std::vector<int> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = v1[i] + v2[i];
         return res;
     }
     
-    const std::vector<double> operator+(const std::vector<double> &v1) {
+    std::vector<double> operator+(const std::vector<double> &v1) {
         return v1;
     }
     
-    const std::vector<int> operator+(const std::vector<int> &v1) { return v1; }
+    std::vector<int> operator+(const std::vector<int> &v1) { return v1; }
     
-    const std::vector<double> operator-(const std::vector<double> &v1,
-                                        const std::vector<double> &v2) {
+    std::vector<double> operator-(const std::vector<double> &v1,
+                                  const std::vector<double> &v2) {
         std::vector<double> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = v1[i] - v2[i];
         return res;
     }
     
-    const std::vector<int> operator-(const std::vector<int> &v1,
-                                     const std::vector<int> &v2) {
+    std::vector<int> operator-(const std::vector<int> &v1,
+                               const std::vector<int> &v2) {
         std::vector<int> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = v1[i] - v2[i];
         return res;
     }
     
-    const std::vector<double> operator-(const std::vector<double> &v1) {
+    std::vector<double> operator-(const std::vector<double> &v1) {
         std::vector<double> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = -v1[i];
         return res;
     }
     
-    const std::vector<int> operator-(const std::vector<int> &v1) {
+    std::vector<int> operator-(const std::vector<int> &v1) {
         std::vector<int> res(v1.size());
         for (int i = 0; i < v1.size(); ++i)
             res[i] = -v1[i];
         return res;
     }
     
-    const double operator*(const std::vector<double> &v1,
+    double operator*(const std::vector<double> &v1,
                            const std::vector<double> &v2) {
         double res = 0;
         for (int i = 0; i < v1.size(); ++i)
@@ -69,7 +69,7 @@ namespace task {
         return res;
     }
     
-    const int operator*(const std::vector<int> &v1, const std::vector<int> &v2) {
+    int operator*(const std::vector<int> &v1, const std::vector<int> &v2) {
         int res = 0;
         for (int i = 0; i < v1.size(); ++i)
             res += v1[i] * v2[i];
@@ -186,16 +186,15 @@ namespace task {
     }
     
     template <typename T> void reverse(std::vector<T> &v) {
-        typename std::vector<T>::iterator first = v.begin();
-        typename std::vector<T>::iterator last = v.end();
-        while ((first!=last)&&(first!=--last)) {
-            std::swap (*first, *last);
-            ++first;
+        int last = v.size() - 1;
+        for (int i = 0; i < v.size()/2; ++i) {
+            std::swap (v[i], v[last]);
+            --last;
         }
     }
     
-    const std::vector<int> operator|(const std::vector<int> &v1,
-                                     const std::vector<int> &v2) {
+    std::vector<int> operator|(const std::vector<int> &v1,
+                               const std::vector<int> &v2) {
         std::vector<int> res(v1.size());
         for (int i = 0; i < v1.size(); ++i) {
             res[i] = v1[i] | v2[i];
@@ -203,8 +202,8 @@ namespace task {
         return res;
     }
     
-    const std::vector<int> operator&(const std::vector<int> &v1,
-                                     const std::vector<int> &v2) {
+    std::vector<int> operator&(const std::vector<int> &v1,
+                               const std::vector<int> &v2) {
         std::vector<int> res(v1.size());
         for (int i = 0; i < v1.size(); ++i) {
             res[i] = v1[i] & v2[i];
