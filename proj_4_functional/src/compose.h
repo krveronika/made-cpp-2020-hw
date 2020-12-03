fb00b6b841425595ff65bdce79f155ceafdeba43
Veronika Kruglikova <krveronika@yandex.ru>
2020-12-04 01:13:32 +0300

    clang-format

--- proj_4_functional/src/compose.h
+++ proj_4_functional/src/compose.h
@@ -1,27 +1,15 @@
 #include <functional>
 #include <iostream>
 
-
-template <typename R, typename T>
-auto compose(std::function<R(T)> f)
-{
-    return [=](T & x)
-    {
-        return f(x);
-    };
+template <typename R, typename T> auto compose(std::function<R(T)> f) {
+  return [=](T &x) { return f(x); };
 }
 
 template <typename A, typename B, typename C>
-std::function<A(C)> compose(std::function<A(B)> f, std::function<B(C)> g)
-{
-    return [f,g](C x)
-    {
-        return f(g(x));
-    };
+std::function<A(C)> compose(std::function<A(B)> f, std::function<B(C)> g) {
+  return [f, g](C x) { return f(g(x)); };
 }
 
-template <typename F, typename... Args>
-auto compose(F f, Args&&... args)
-{
-    return compose(f , compose(args...));
+template <typename F, typename... Args> auto compose(F f, Args &&... args) {
+  return compose(f, compose(args...));
 }
--- proj_4_functional/src/main.cpp
+++ proj_4_functional/src/main.cpp
@@ -1,74 +1,68 @@
-#include "fuction.h"
 #include "compose.h"
+#include "fuction.h"
 
-
-int func2(const int * x, int y)
-{
-    return (*x) + y;
-}
+int func2(const int *x, int y) { return (*x) + y; }
 
 using std::cout;
 using std::endl;
 
-void check1()
-{
-    typedef function<int (const int * , int)> int_function_with_two_args_t;
-    int_function_with_two_args_t f2(func2);
-    
-    int x = 10;
-    cout << "calling function with signature int (const int * , int):              " <<  f2(&x, 20) << endl;
+void check1() {
+  typedef function<int(const int *, int)> int_function_with_two_args_t;
+  int_function_with_two_args_t f2(func2);
+
+  int x = 10;
+  cout << "calling function with signature int (const int * , int):            "
+          "  "
+       << f2(&x, 20) << endl;
 }
 
+int main(int argc, const char *argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  // check1();
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    //check1();
+  const std::function<bool(int)> a = [](int x) -> bool {
+    std::cout << "a" << std::endl;
+    return x > 10;
+  };
 
-    const std::function<bool(int)> a = [] (int x) -> bool {
-        std::cout << "a" << std::endl;
-        return x > 10;
-    };
-    
-    const std::function<int(float)> b = [] (float y) -> int {
-        std::cout << "b" << std::endl;
-        return int(y * y);
-    };
-    
-    const std::function<float(bool)> c = [] (bool z) -> float {
-        std::cout << "c" << std::endl;
-        return z ? 3.1f : 3.34f;
-    };
-    
+  const std::function<int(float)> b = [](float y) -> int {
+    std::cout << "b" << std::endl;
+    return int(y * y);
+  };
 
-    {
-        cout << "Test 1" << endl;
-        float x = 9;
-        auto d = compose(b);
-        auto true_res = b(x);
-        if (d(x) != true_res) {
-            cout << "Test Failed" << endl;
-        }
+  const std::function<float(bool)> c = [](bool z) -> float {
+    std::cout << "c" << std::endl;
+    return z ? 3.1f : 3.34f;
+  };
+
+  {
+    cout << "Test 1" << endl;
+    float x = 9;
+    auto d = compose(b);
+    auto true_res = b(x);
+    if (d(x) != true_res) {
+      cout << "Test Failed" << endl;
     }
-    
-    {
-        cout << "Test 2" << endl;
-        float x = 11;
-        auto d = compose(a, b);
-        auto true_res = a(b(x));
-        if (d(x) != true_res) {
-            cout << "Test Failed" << endl;
-        }
-    }
-    
-    {
-        cout << "Test 3" << endl;
-        auto d = compose(a, b, c, a, b, c);
-        auto true_res = a(b(c(a(b(c(true))))));
-        if (d(true) != true_res) {
-            cout << "Test Failed" << endl;
-        }
-    }
-    return 0;
+  }
 
+  {
+    cout << "Test 2" << endl;
+    float x = 11;
+    auto d = compose(a, b);
+    auto true_res = a(b(x));
+    if (d(x) != true_res) {
+      cout << "Test Failed" << endl;
+    }
+  }
+
+  {
+    cout << "Test 3" << endl;
+    auto d = compose(a, b, c, a, b, c);
+    auto true_res = a(b(c(a(b(c(true))))));
+    if (d(true) != true_res) {
+      cout << "Test Failed" << endl;
+    }
+  }
+  return 0;
 }
