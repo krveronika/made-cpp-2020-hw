8e9b758a90646b276bc397b72fc204f11fd4be14
Veronika Kruglikova <krveronika@yandex.ru>
2020-12-04 01:36:03 +0300

    Проверка типов в шаблоне

--- proj_4_functional/src/compose.h
+++ proj_4_functional/src/compose.h
@@ -10,6 +10,7 @@ std::function<A(C)> compose(std::function<A(B)> f, std::function<B(C)> g) {
   return [f, g](C x) { return f(g(x)); };
 }
 
-template <typename F, typename... Args> auto compose(F f, Args &&... args) {
-  return compose(f, compose(args...));
+template <typename A, typename B, typename C, typename... Args>
+auto compose(std::function<A(B)> f, std::function<B(C)> g, Args &&... args) {
+  return compose(f(g), compose(args...));
 }
